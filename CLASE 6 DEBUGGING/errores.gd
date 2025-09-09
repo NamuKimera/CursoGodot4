b569aa5f33b5196550250014af4d8f2460a0003a
NamuKimeraVT <namuquimera@gmail.com>
2025-09-09 09:54:18 -0300

    Clase 6

---
+++ CLASE 6 DEBUGGING/errores.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# Variables
+var vidas : int
+var demoArray : Array = [1, 12, 20]
+
+
+func _ready() -> void:
+	if vidas > 5:
+		pass
+	
+	var Node
+	
+	#vidas = -1
+	#print(vidas)
+	
+	#var player = get_node("Player")
+	#player.jump()
+	
+	print(demoArray[3])
